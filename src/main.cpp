9aa1256ccdcafe793b2fc9b42b8b2d5b83587503
Pietra <73311085+pijuma@users.noreply.github.com>
2025-06-19 21:54:56 -0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -5,27 +5,22 @@
 
 // slow.gmelodie.com:7033
 
-#define SERVER_IP "slow.gmelodie.com"
+#define SERVER_IP "142.93.184.175"
 #define SERVER_PORT 7033
 
 int main() {
-    const string serverIp = "slow.gmelodie.com";
+    const string serverIp = "142.93.184.175";
     uint16_t serverPort = 7033;
 
     UDPSocket socket;
     ProtocolHandler pc;
     bool checkCom;
 
-
-    if(!socket.connectTo(serverIp, serverPort))
-        cout << "Erro" << endl;
-
-    /*
     checkCom = pc.handshake(socket, SERVER_IP, SERVER_PORT);
     if(checkCom)
         cout << "Yay" << endl;
     else
         cout << "Tristeza" << endl;
-    */
+
     return 0;
 }
